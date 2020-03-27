4475500d11fbb75253b895e44ed88b9114b6a3c9
zhangyan <1099694846@qq.com>
2020-03-27 18:02:02 +0800

    弄

--- es6/class.js
+++ es6/class.js
@@ -8,11 +8,13 @@
 
 // es6写法
 
-class Animal {
+/*class Animal {
 	constructor (name, food) {
 		this.name = name
 		this.food = food
 	}
+
+	// 为Animal的原型加上eatWhat方法
 	eatWhat () {
 		console.log(`${this.name} eat ${this.food}`)
 	}
@@ -31,14 +33,16 @@ elephant.eatWhat()
 
 class Monkey extends Animal {
 	constructor (name, food) {
-		super (name, food)
+		super (name, food) //继承父类的属性，必须写在任何this的开头，不然会报错
 	}
 	feature () {
 		console.log(this.name)
 	}
+
+	//重写原型方法
 	eatWhat () {
-		// console.log('eat what')
-		super.eatWhat()
+		console.log('eat what')
+		// super.eatWhat()
 	}
 }
 
@@ -50,13 +54,7 @@ monkey01.eatWhat()
 
 Animal.canTalk()
 
-
-
-
-
-
-
-
+*/
 
 
 
@@ -70,7 +68,8 @@ Animal.canTalk()
  */
 
 
-// es5 写法 
+
+// 创建一个构造函数作为原型
 
 function Animals (name,food) {
 	this.name = name
@@ -78,20 +77,49 @@ function Animals (name,food) {
 	this.sayHello = function () {
 		console.log(`my name is ${this.name}`)
 	}
+
 }
+
+
+//加上静态方法
+Animals.isAnimal = function isAnimal() {
+	console.log('i don\'t know ')
+}
+
+// 在原型上添加一个方法( es5 )
 Animals.prototype.eatWhat = function () {
 	console.log(`${this.name} eat ${this.food}`)
 }
+
 
 let tiger = new Animals('tiger', 'meat')
 
 tiger.eatWhat()
 
 
-// es5 如何继承？
+/* 
+	es5 如何继承？ 先创建一个 tigerClass 子类（也就是构造函数),
+
+	在调用call方法将 父类的 this指向 tigerClass， 这样 tigerClass 就拥有了
+
+	父类 this.xxx 的所有属性；
+
+	但是 父类prototype上面的挂载的东西怎么继承过来呢？ 
+
+	因为tigerClass 不是通过 new Animals() 创建的 一个实例，所以无法通过 原型链调用 Animals原型上挂载的东西
+
+
+	我们 可以通过创建一个 新的拥有 Animals原型 所有属性 和 方法的 对象 挂载到 tigerClass 的原型上，这样 new tigerClass()
+
+	创建的实例就可以调用和访问了:
+
+	tigerClass.prototype = Object.create(Animal.prototype)
+
+
+
+*/
 
 function tigerClass (name, food, legs) {
-
 	/*
 		A.call(B,x,y)
 
@@ -109,12 +137,20 @@ function tigerClass (name, food, legs) {
 	this.legs = legs
 }
 
-// tigerClass.prototype = new Animals()
+tigerClass.prototype = Object.create(Animals.prototype)
 
-let tiger01 = new tigerClass('tiger', 'meat', 4)
+
+let tiger01 = new tigerClass('tiger01', 'meat', 4)
 
 console.log(tiger01.legs)
 console.log(tiger01.name)
 
-// tiger01.eatWhat()
-tiger01.sayHello()
+// eatWhat 是 Animals 原型挂载的方法，无法通过原型链查找到的， 除非 tigerClass.prototype = Object.create(Animals.prototype)
+
+tiger01.eatWhat()  
+
+tiger01.sayHello() // my name is huangj
+
+// tiger01.isAnimal()  //报错因为 isAnimal 是 Animals 的静态方法，只能由 Animals 调用
+
+Animals.isAnimal()
